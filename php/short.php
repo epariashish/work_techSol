e0b78dffc0d8245c9c80ce72b7e2c1f5043eb24e
epariashish <ekumarashish10@gmail.com>
2019-12-25 22:58:58 +0530

    my solution

---
+++ php/short.php
@@ -0,0 +1,12 @@
+<h1>array shorthing</h1>
+<?php
+$sort =array(1,5,4,6,9,2);
+print_r($sort);
+?>
+<?php
+$food =array('razz','apple','banana','orange','grape');
+//sort($food);
+echo"<pre>"
+print_r($food);
+echo"</pre>";
+?>
